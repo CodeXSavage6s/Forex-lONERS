7b1ad431222cb139e12292710484a93cf075cc7a
Code X Savage <iamsavage113377@gmail.com>
2026-04-28 00:56:05 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,34 @@
+console.log("best ")
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+   (async () => {
+    try {
+    const cache = await caches.open('test-pwa')
+    await cache.addAll([
+     '/',
+     '/index.html',
+     '/src/style.css',
+     'src/script.js'
+    ])
+    self.skipWaiting()
+    console.log('Cached Successful', cache)
+    } catch (err) {
+     console.error('Error msg', err)
+    }
+   })()
+  )
+})
+
+self.addEventListener('activate', (event) => {
+ console.log("testing activate")
+})
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      const response = await caches.match(event.request);
+      return response || fetch(event.request);
+    })()
+  );
+});
